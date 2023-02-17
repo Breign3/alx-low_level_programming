f6e6657d30dbeec21a97b6f6760d1865b21bef45
Breign3 <brainy.ngwenya@gmail.com>
2023-02-16 18:10:20 -0800

    5-printf.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description :  prints a line with puts fuction
+ * Return : 0 Always (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
