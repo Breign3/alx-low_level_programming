b90dc951bfc0d76eaf1c91b60f2a991b833140e3
Breign3 <brainy.ngwenya@gmail.com>
2023-02-23 15:06:31 -0800

    0-isupper.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,14 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _isupper - checks for uppercase character
- * @c: the character to be checked
+ * _isupper - a function that checks for uppercase character
+ * @c: input
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	if (c >= 65 && c <= 90)
-	{
+	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
-	}
-	return (0); 
+	else
+		return (0);
 }
