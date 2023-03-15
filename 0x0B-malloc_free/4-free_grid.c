2ffc466299b52bc3f915683a56d6ad3d760eca39
Breign3 <brainy.ngwenya@gmail.com>
2023-03-14 17:20:56 -0700

    free_grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-
 /**
  * free_grid - frees 2d array
  * @grid: 2d grid
@@ -14,6 +13,7 @@
 void free_grid(int **grid, int height)
 {
 	int i;
+
 	for (i = 0; i < height; i++)
 	{
 		free(grid[i]);
