0b4d3b8639896b3d1ad2204d278310677c8b3270
Breign3 <brainy.ngwenya@gmail.com>
2023-03-15 14:09:51 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,37 +6,34 @@
  * @s2: input two to concat
  * Return: concat of s1 and s2
  */
-char *str_concat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *conct;
-	int i, ci;
+	char *str;
+	unsigned int i, a, b;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	a = b = 0;
+	if (s1)
+	for (; s1[a]; a++)
+	;
+		if (s2)
+		for (; s2[b]; b++)
+	;
+			if (!(n >= b))
+			b = n;
 
-	i = ci = 0;
-	while (s1[i] != '\0')
-		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
+		str = malloc(sizeof(char) * (a + b) + 1);
+			if (!str)
+			return (NULL);
 
-	if (conct == NULL)
-		return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
+				for (i = 0; i < (a + b); i++)
+					{
+					if (i < a)
+						str[i] = s1[i];
+							else
+								str[i] = s2[i - a];
+					}
 
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+								str[i] = '\0';
+
+		return (str);
 }
