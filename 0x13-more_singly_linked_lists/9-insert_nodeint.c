23262805f740af83ea80d3e9c3bab7211459173d
Breign3 <brainy.ngwenya@gmail.com>
2023-04-05 18:56:19 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,46 +1,48 @@
 #include "lists.h"
 
 /**
- * insert_nodeint_at index - insert a new node at a given position
- * @head: pointer to the struct
- * @n: integer in struct
- * @idx: index of the list where the node should be added
- * Return: address or NULL
+ * insert_nodeint_at_index - inserts a new node
+ * at a given position.
+ * @head: head of a list.
+ * @idx: index of the list where the new node is
+ * added.
+ * @n: integer element.
+ *
+ * Return: the address of the new node, or NULL if it
+ * failed.
  */
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *newnode, *move = *head;
-	unsigned int index;
+	unsigned int i;
+	listint_t *new;
+	listint_t *temp = *head;
 
-	newnode = malloc(sizeof(listint_t));
-
-	if (newnode == NULL)
-	{
+	new = malloc(sizeof(listint_t));
+	if (!new || !head)
 		return (NULL);
-	}
 
-	newnode->n = n;
+	new->n = n;
+	new->next = NULL;
 
 	if (idx == 0)
 	{
-		newnode->next = *head;
-		*head = newnode;
+		new->next = *head;
+		*head = new;
+		return (new);
+	}
 
-	}
-	else
+	for (i = 0; temp && i < idx; i++)
 	{
-		for (index = 0 ; index - 1 ; index++)
-			{
-				move = move->next;
-				if (move == NULL)
-				{
-					free(newnode);
-					return (NULL);
-				}
-			}
-			newnode->next = move->next;
-			move->next = newnode;
+		if (i == idx - 1)
+		{
+			new->next = temp->next;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
 	}
-	return (newnode);
+
+	return (NULL);
 }
