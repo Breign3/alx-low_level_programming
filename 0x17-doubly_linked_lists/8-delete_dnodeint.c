c18609ab2eaf4b88137cccd1b8fa6df136922297
Breign3 <brainy.ngwenya@gmail.com>
2023-08-18 18:32:49 -0700

    8-delete_dnodeint.c

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -1,50 +1,65 @@
 #include "lists.h"
+#include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - function that deletes the node at index of a,
- * linked list.
- * @head: pointer to pointer to the h of linked list.
- * @index: ndex of the node that should be deleted. Index starts at 0.
- *
- * Return: if it succeeded, -1 if it failed
+ * delete_dnodeint_at_index - delete node at give index
+ * @head:list
+ * @index:given index
+ * Return: -1 or 0
  */
-
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *head1;
-	dlistint_t *head2;
-	unsigned int count;
+	dlistint_t *start;
+	unsigned int i;
+	unsigned int len;
+	len = len_node(&head);
 
-	head1 = *head;
-
-	if (head1 != NULL)
-		while (head1->prev != NULL)
-			head1 = head1->prev;
-	count = 0;
-
-	while (head1 != NULL)
+	start = *head;
+	if (*head == NULL)
+		return (-1);
+	if (index == 0)
 	{
-		if (count == index)
-		{
-			if (count == 0)
-			{
-				*head = head1->next;
-				if (*head != NULL)
-					(*head)->prev = NULL;
-			}
-			else
-			{
-				head2->next = head1->next;
-
-				if (head1->next != NULL)
-					head1->next->prev = head2;
-			}
-			free(head1);
-			return (1);
-		}
-		head2 = head1;
-		head1 = head1->next;
-		count++;
+		start = start->next;
+		free(*head);
+		*head = start;
+		if (start != NULL)
+			start->prev = NULL;
+		return (1);
 	}
-	return (-1);
+	for (i = 0; i <= index - 1; i++)
+	{
+		start = start->next;
+		if (!start)
+			return (-1);
+	}
+	if (len - 1 == index)
+	{
+		start->prev->next = NULL;
+		free(start);
+		return (1);
+	}
+	start->prev->next = start->next;
+	start->next->prev = start->prev;
+	free(start);
+	return (1);
+}
+
+/**
+ * len_node - list len
+ *
+ * @node:list
+ * Return:unsigned int
+ */
+unsigned int len_node(dlistint_t **node)
+{
+	unsigned int len = 0;
+	dlistint_t *start;
+
+	start = *node;
+	while (start != NULL)
+	{
+		len += 1;
+		start = start->next;
+	}
+	return (len);
 }
